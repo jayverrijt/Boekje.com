4889546863e94d22705f2fd919df3f2e5ea429b5
Jay Verrijt <109523337+jayverrijt@users.noreply.github.com>
2026-04-11 11:53:12 +0200

    Chore: Migrated the user auth service from ef core to mysql ocnnector

--- Boekje.Domain/Entities/User.cs
+++ Boekje.Domain/Entities/User.cs
@@ -3,6 +3,8 @@ namespace Boekje.Domain.Entities;
 public class User
 {
     public int Id { get; set; }
-    public string Email { get; set; } = string.Empty;
-    public string PasswordHash { get; set; } = string.Empty;
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
+
 }
