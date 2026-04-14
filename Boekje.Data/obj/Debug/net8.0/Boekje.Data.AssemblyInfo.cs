92e718f58b212a75620e8936e65c76d4c99ce9e5
Jay Verrijt <109523337+jayverrijt@users.noreply.github.com>
2026-04-14 09:39:23 +0200

    Update

--- Boekje.Data/obj/Debug/net8.0/Boekje.Data.AssemblyInfo.cs
+++ Boekje.Data/obj/Debug/net8.0/Boekje.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Boekje.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c70d11fbea0862036359c3e669f47faac5fcdd4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+675ce5982341753c4d3a9726438348f5696ed938")]
 [assembly: System.Reflection.AssemblyProductAttribute("Boekje.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Boekje.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Boekje.Web/obj/Debug/net8.0/Boekje.Web.AssemblyInfo.cs
+++ Boekje.Web/obj/Debug/net8.0/Boekje.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Boekje.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c70d11fbea0862036359c3e669f47faac5fcdd4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+675ce5982341753c4d3a9726438348f5696ed938")]
 [assembly: System.Reflection.AssemblyProductAttribute("Boekje.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Boekje.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
