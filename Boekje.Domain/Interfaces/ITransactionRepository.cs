7eb69cbef18c949ef704c6e4850915cf0ecacd66
Jay Verrijt <109523337+jayverrijt@users.noreply.github.com>
2026-04-27 19:16:32 +0200

    Chore uploaded backend

---
+++ Boekje.Domain/Interfaces/ITransactionRepository.cs
@@ -0,0 +1,9 @@
+using Boekje.Domain.Entities;
+
+namespace Boekje.Domain.Interfaces;
+
+public interface ITransactionRepository
+{
+    void AddTransaction(Transaction transaction);
+    List<Transaction> GetByUser(int userId);
+}
