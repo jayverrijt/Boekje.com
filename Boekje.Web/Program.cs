7aac573d71709a6aa9392bd7411bc096b35f5c9c
Jay Verrijt <109523337+jayverrijt@users.noreply.github.com>
2026-05-26 11:11:08 +0200

    Added env file

--- Boekje.Web/Program.cs
+++ Boekje.Web/Program.cs
@@ -1,6 +1,17 @@
 using Boekje.Web;
+using DotNetEnv;
+
+Env.Load();
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration["ConnectionStrings:DefaultConnection"] =
+    $"Server={Environment.GetEnvironmentVariable("DB_SERVER")};" +
+    $"Port={Environment.GetEnvironmentVariable("DB_PORT")};" +
+    $"Database={Environment.GetEnvironmentVariable("DB_NAME")};" +
+    $"Uid={Environment.GetEnvironmentVariable("DB_USER")};" +
+    $"Pwd={Environment.GetEnvironmentVariable("DB_PASSWORD")};" +
+    "Pooling=false;";
 
 var startup = new Startup(builder.Configuration);
 
